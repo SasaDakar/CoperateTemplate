011784c82a6a976f84081026c3ac489f69946b22
SasaDakar <sakolwat.nak@gmail.com>
2025-02-10 01:32:36 +0700

    add new files

---
+++ PythonProject1/main01.py
@@ -0,0 +1,12 @@
+
+import tkinter as tk
+
+
+ window = tk.Tk()
+ window.title('Justpython')
+ window.minsize(width=400, height=800)
+
+title_label = tk.label(master=window, text= ID citizen)
+title_label.pack()
+
+ window.mainloop()
